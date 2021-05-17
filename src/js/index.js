6490f3cdbd88e6d66b20c254ddb49a7ddb910330
Zileks <zilebre93@gmail.com>
2021-05-17 12:29:01 +0200

    fixed some bugs

--- src/js/index.js
+++ src/js/index.js
@@ -18,43 +18,51 @@ window.onclick = function(event) {
   }
 }
 
-var p1 = document.getElementById('price1');
-var text1 = p1.textContent.replace(',','.').slice(0,6)
-var price1 = Number(text1)
-
-var p2 = document.getElementById('price2');
-var text2 = p2.textContent.replace(',','.').slice(0,6)
-var price2 = Number(text2)
-
-var p3 = document.getElementById('price3');
-var text3 = p3.textContent.replace(',','.').slice(0,6)
-var price3 = Number(text3)
-
-var p4 = document.getElementById('price4');
-var text4 = p4.textContent.replace(',','.').slice(0,6)
-var price4 = Number(text4)
-
-console.log(price1,price2,price3,price4)
-
-var c1 = document.getElementById('check1').value
 
 
-console.log(c1)
+
+
+
 
 function myFunction() {
-  var x = document.getElementById("check1").value;
-  document.getElementById("demo").innerHTML = x;
-}
+  var isChecked1 = document.getElementById('check1').checked;
+  var isChecked2 = document.getElementById('check2').checked;
+  var isChecked3 = document.getElementById('check3').checked;
+  var isChecked4 = document.getElementById('check4').checked;
+  console.log(isChecked1,isChecked2,isChecked3,isChecked4);
 
-function handleChange() {
-  var x = document.getElementById("check1").value;
-  console.log(x)
-}
-
-var totalprice = 39.999 + 
+  if(isChecked1){
+    var price1 = 278.6;
+  }
+  else{
+      price1 = 0;
+  }
+  if(isChecked2){
+    var price2 = 558.78;
+  }
+  else{
+      price2 = 0;
+  }
+  if(isChecked2){
+    var price3 = 357.29;
+  }
+  else{
+      price3 = 0;
+  }
+  if(isChecked3){
+    var price4 = 637.48;
+  }
+  else{
+      price4 = 0;
+  }
+  
+  var price = 39999;
+  var totalValue = price + price1 + price2 + price3 + price4;
+  
+} 
 
 imageCarousel();
 
-handleChange();
+myFunction();
 
 
